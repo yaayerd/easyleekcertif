c4908b0b917bb3088c5f25b203836412815aeb3a
Yaye Rokhaya <ladyrokhaya@gmail.com>
2024-01-29 11:44:50 +0000

    Mise en place des middlewares dans api.php

--- routes/api.php
+++ routes/api.php
@@ -27,86 +27,109 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Les routes d'authentification de user
-Route::post('/user/register', [UserController::class, 'userRegister'] );
-Route::post('/user/login', [UserController::class, 'userLogin'] );
-Route::post('/user/modify/profile/{user}', [UserController::class, 'userModifyProfile'] );
-Route::post('/user/logout', [UserController::class, 'userLogout']);
-Route::post('/user/me', [UserController::class, 'userMe']);
-
 Route::group([
     ['middleware' => 'auth:user-api'],
     'prefix' => 'auth'
 ], function ($router) {
-
-    
 });
 
-// Les routes d'authentification de restaurant
-Route::post('/restaurant/register', [UserController::class, 'restaurantRegister'] );
-Route::post('/restaurant/login', [UserController::class, 'restaurantLogin'] );
-Route::post('/restaurant/modify/profile/{restaurant}', [UserController::class, 'restautantModifyProfile'] );
-Route::post('/restaurant/logout', [UserController::class, 'restaurantLogout']);
-Route::post('/restaurant/me', [UserController::class, 'restaurantMe']);
-
-// --------------------  Les routes liées à Catégorie 
-
-Route::get('/categorie/list', [CategorieController::class, 'index'] );
-Route::post('/categorie/store', [CategorieController::class, 'store'] );
-Route::put('/categorie/update/{id}', [CategorieController::class, 'update']);
-Route::get('/categorie/show/{id}', [CategorieController::class, 'show']);
-Route::delete('/categorie/delete/{id}', [CategorieController::class, 'destroy']);
 
 
-// --------------------  Les routes liées au Menu 
+// **********************Les routes libres à tous users ***************************
 
-Route::get('/menu/list', [MenuController::class, 'index'] );
-Route::post('/menu/store', [MenuController::class, 'store'] );
-Route::put('/menu/update/{id}', [MenuController::class, 'update']);
-Route::get('/menu/show/{id}', [MenuController::class, 'show']);
-Route::delete('/menu/delete/{id}', [MenuController::class, 'destroy']);
-
-
-// --------------------  Les routes liées au Plat 
-
-Route::get('/plat/list', [PlatController::class, 'index'] );
-Route::post('/plat/store', [PlatController::class, 'store'] );
-Route::put('/plat/update/{id}', [PlatController::class, 'update']);
-Route::patch('/plat/archiver/{id}', [PlatController::class, 'archiver']);
-Route::patch('/plat/desarchiver/{id}', [PlatController::class, 'desarchiver']);
-Route::get('/plat/show/{id}', [PlatController::class, 'show']);
-Route::delete('/plat/delete/{id}', [PlatController::class, 'destroy']);
+Route::post('/restaurant/register', [UserController::class, 'restaurantRegister']);
+Route::post('/restaurant/login', [UserController::class, 'restaurantLogin']);
+Route::post('/user/register', [UserController::class, 'userRegister']);
+Route::post('/user/login', [UserController::class, 'userLogin']);
+// -----------------------------Libres ---------------------
+Route::get('/categorie/list', [CategorieController::class, 'index']);
+Route::get('/menu/list', [MenuController::class, 'index']);
+Route::get('/avis/list', [AvisController::class, 'index']);
+Route::get('/plat/list', [PlatController::class, 'index']);
 
 
-// --------------------  Les routes liées à Commande pour le User
-
-Route::get('/commande/list', [CommandeController::class, 'index'] );
-Route::post('/commande/store', [CommandeController::class, 'store'] );
-Route::put('/commande/update/{commande}', [CommandeController::class, 'updateCommande']);
-Route::get('/commande/show/{id}', [CommandeController::class, 'show']);
-Route::delete('/commande/annuler/{id}', [CommandeController::class, 'annulerCommande']);
 
 
-// --------------------  Les routes liées à Commande pour le restaurant
-
-Route::put('/commande/refuser/{id}', [CommandeController::class, 'refuserCommande']);
-Route::put('/commande/accepter/{id}', [CommandeController::class, 'accepterCommande']);
 
 
-// --------------------  Les routes liées aux Avis 
+// Les routes de l' AdminSystem**************************************************
 
-Route::get('/avis/list', [AvisController::class, 'index'] );
-Route::post('/avis/store', [AvisController::class, 'store'] );
-Route::put('/avis/update/{id}', [AvisController::class, 'update']);
-Route::get('/avis/show/{id}', [AvisController::class, 'show']);
-Route::delete('/avis/delete/{id}', [AvisController::class, 'destroy']);
+Route::group(['prefix' => 'auth', 'middleware' => ['auth:user-api', 'adminSystem']], function () {
+
+    // --------------------  Les routes liées à Catégorie 
+
+    Route::post('/categorie/store', [CategorieController::class, 'store']);
+    Route::put('/categorie/update/{id}', [CategorieController::class, 'update']);
+    Route::get('/categorie/show/{id}', [CategorieController::class, 'show']);
+    Route::delete('/categorie/delete/{id}', [CategorieController::class, 'destroy']);
+
+    // --------------------  Les routes liées aux Rôles 
+
+    Route::get('/role/list', [RoleController::class, 'index']);
+    Route::post('/role/store', [RoleController::class, 'store']);
+    Route::put('/role/update/{id}', [RoleController::class, 'update']);
+    Route::get('/role/show/{id}', [RoleController::class, 'show']);
+    Route::delete('/role/delete/{id}', [RoleController::class, 'destroy']);
+});
 
 
-// --------------------  Les routes liées aux Rôles 
+// Les routes  pour le restaurant**************************************
 
-Route::get('/role/list', [RoleController::class, 'index'] );
-Route::post('/role/store', [RoleController::class, 'store'] );
-Route::put('/role/update/{id}', [RoleController::class, 'update']);
-Route::get('/role/show/{id}', [RoleController::class, 'show']);
-Route::delete('/role/delete/{id}', [RoleController::class, 'destroy']);
+Route::group(['prefix' => 'auth', 'middleware' => ['auth:user-api', 'restaurant']], function () {
 
+    // ------------------Route pour le restaurant sur son profil du restaurant 
+
+    Route::post('/restaurant/modify/profile/{restaurant}', [UserController::class, 'restautantModifyProfile']);
+    Route::post('/restaurant/logout', [UserController::class, 'restaurantLogout']);
+    Route::post('/restaurant/me', [UserController::class, 'restaurantMe']);
+
+    // --------------------  Les routes liées au Menu 
+
+    Route::post('/menu/store', [MenuController::class, 'store']);
+    Route::put('/menu/update/{id}', [MenuController::class, 'update']);
+    Route::get('/menu/show/{id}', [MenuController::class, 'show']);
+    Route::delete('/menu/delete/{id}', [MenuController::class, 'destroy']);
+
+    // --------------------  Les routes liées au Plat 
+
+    Route::post('/plat/store', [PlatController::class, 'store']);
+    Route::put('/plat/update/{id}', [PlatController::class, 'update']);
+    Route::patch('/plat/archiver/{id}', [PlatController::class, 'archiver']);
+    Route::patch('/plat/desarchiver/{id}', [PlatController::class, 'desarchiver']);
+    Route::get('/plat/show/{id}', [PlatController::class, 'show']);
+    Route::delete('/plat/delete/{id}', [PlatController::class, 'destroy']);
+
+    // --------------------  Les routes liées à Commande pour le restaurant
+    
+    Route::get('/commande/list', [CommandeController::class, 'index']);
+    Route::put('/commande/refuser/{id}', [CommandeController::class, 'refuserCommande']);
+    Route::put('/commande/accepter/{id}', [CommandeController::class, 'accepterCommande']);
+});
+
+
+
+// Les routes du Client ********************************************
+
+Route::group(['prefix' => 'auth', 'middleware' => ['auth:user-api', 'adminSystem']], function () {
+
+    // Les routes d'authentification de user
+
+    Route::post('/user/modify/profile/{user}', [UserController::class, 'userModifyProfile']);
+    Route::post('/user/logout', [UserController::class, 'userLogout']);
+    Route::post('/user/me', [UserController::class, 'userMe']);
+
+    // --------------------  Les routes liées à Commande pour le User
+
+    Route::get('/commande/list', [CommandeController::class, 'index']);
+    Route::post('/commande/store', [CommandeController::class, 'store']);
+    Route::put('/commande/update/{commande}', [CommandeController::class, 'updateCommande']);
+    Route::get('/commande/show/{id}', [CommandeController::class, 'show']);
+    Route::delete('/commande/annuler/{id}', [CommandeController::class, 'annulerCommande']);
+
+    // --------------------  Les routes liées aux Avis 
+
+    Route::post('/avis/store', [AvisController::class, 'store']);
+    Route::put('/avis/update/{id}', [AvisController::class, 'update']);
+    Route::get('/avis/show/{id}', [AvisController::class, 'show']);
+    Route::delete('/avis/delete/{id}', [AvisController::class, 'destroy']);
+});
